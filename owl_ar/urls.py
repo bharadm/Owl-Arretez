ed8b160bd76f90b09af3fb7828bfc6be0632b28e
Bharadwaj M <bharadm12@gmail.com>
2020-03-20 19:28:22 +0530

    Made local navigation as parent to provide inheritance.

--- owl_ar/urls.py
+++ owl_ar/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('index', include('index_page.urls')),
     path('admin/', admin.site.urls),
+    path('item', include('item_display.urls'))
 ]
